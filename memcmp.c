177d31334dd345a5e0804a3c2942e3e25ff5a2c1
kaoutharBousbaa1 <kaouthar.bousbaa@um6p.ma>
2022-01-01 15:56:14 +0100

    add memcmp

---
+++ memcmp.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    size_t i = 0;
+    while(i < n)
+    {
+        if(((unsigned char*)s1)[i] != ((unsigned char*)s2)[i])
+        {
+            return ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i];
+        }
+        i++;
+    }
+    return 0;
+}
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
